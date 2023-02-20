c869119352e8f6dff3d38450799fc006f5c34050
SamPirzad <125896893+SamPirzad@users.noreply.github.com>
2023-02-20 19:19:59 +0330

    Create OutPut_Types.cs

---
+++ OutPut_Types.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutPut_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World!");
+            Console.WriteLine(" How");
+            Console.WriteLine("are");
+            Console.WriteLine("you?");
+        }
+    }
+}
